9cba4961a970e12874ae53686db71729ac809154
William Murphy <105789980+wmurphywork@users.noreply.github.com>
2023-12-04 14:13:56 +0000

    Day one solution complete!

---
+++ DayOne/DayOne.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+    file, err := os.Open("FileToParse.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+	totalCounter := 0
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+		regexToUse := regexp.MustCompile("[0-9]")
+		intsOnly := regexToUse.FindAllString(scanner.Text(), -1)
+		numberOfInts := (len(intsOnly) - 1)
+		
+		combinedNumber, err := strconv.Atoi(intsOnly[0] + intsOnly[numberOfInts])
+		if err != nil {
+			log.Fatal(err)
+		}
+		totalCounter = totalCounter + combinedNumber
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+	fmt.Println("The answer is...")
+	fmt.Println(totalCounter)
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AdventOfCode2023
-AdventOfCode2023 
+
+This is just a repo containing my solutions for the advent of code 2023. Lets see how long I keep it up for...
+
+Link: https://adventofcode.com/2023
